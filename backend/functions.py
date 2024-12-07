95e235569ec8f99d310b7a21583fde7e5d3a8284
kgushin <kgushin@gmail.com>
2024-12-07 20:19:02 +0300

    Добавил описание структур, скопировал normalize из основной ветки

--- backend/functions.py
+++ backend/functions.py
@@ -1,19 +1,34 @@
-"""
-Structures
+import math
+import struct
+import wave
+import structures
 
 
-# Кусочно-линейная огибающая
-# Массив таплов (Tn, An),
-#  где Tn -- момент времени (если значение отрицательное, отсчитывается от конца звучания)
-#  An -- относительная амплитуда в этот момент времени
-# Пример:
-envelope = [
-    (0,1)
-]
-"""
+SAMPLE_RATE = 44100
 
 
-#Functions
+# Functions
+
+def num_samples(num_seconds: float) -> int:
+    """
+    Перевод секунд в количество сэмплов
+    :param num_seconds: интервал времени, для которого считается количество семплов
+    :return: количество семплов
+    """
+    return int(SAMPLE_RATE * num_seconds)
+
+
+def write_wave(filename, samples):
+    """
+    Запись массива 16-битных сэмплов в режиме моно
+    с частотой дискретизации SR в wav-файл
+    """
+    f = wave.open(filename, "w")
+    f.setparams((1, 2, SAMPLE_RATE, len(samples), "NONE", ""))
+    f.writeframes(b"".join(
+        [struct.pack('<h', x) for x in samples]))
+    f.close()
+
 
 def play_sound(samples: list):
     """
@@ -25,6 +40,7 @@ def validate_params(params: dict):
     """
     Проверяет полноту и корректность словаря параметров синтеза
     """
+    result = False
     return result
 
 
@@ -34,17 +50,33 @@ def save_preset(preset: dict):
     """
 
 
-def load_preset(filename: string) -> dict:
+def load_preset(filename: str) -> dict:
     """
     Загружает набор параметров синтеза из файла
     """
+    preset = {}
     return preset
+
+
+def generate_sine_wave(freq: float, amp: float, duration: float) -> list:
+    """
+    Сгенерировать семплы синусоиды с заданной частотой и длительностью звучания
+    :param freq: float > 0 частота в герцах
+    :param amp: float (0..1) относительная амплитуда
+    :param duration: float > 0 длительность в секундах:
+    :return list: набор семплов
+    """
+    samples = []
+    for sample_no in range(num_samples(duration)):
+        samples.append(math.sin(2 * math.pi * freq * sample_no / SAMPLE_RATE))
+    return samples
 
 
 def modulate_amp(base: list, mod: list) -> list:
     """
     Модулирует базовый сигнал набором значений амплитуды
     """
+    result = []
     return result
 
 
@@ -52,6 +84,7 @@ def mix(sets: list) -> list:
     """
     Смешивает заданные массивы семплов путем почленного суммирования
     """
+    result = []
     return result
 
 
@@ -59,13 +92,25 @@ def apply_envelope(samples: list, envelope: list) -> list:
     """
     Применяет заданную огибающую к набору семплов
     """
+    result = []
     return result
 
 
-def normalize(samples: list, bits_per_sample = 16) -> list:
+def normalize(samples: list, bits_per_sample=16) -> list:
     """
     Нормализует значения семплов в соответствии с разрядностью звукового устройства
     :param samples: массив значений семплов
-    :return:
+    :param bits_per_sample: разрядность звукового устройства
+    :return: массив нормализованных значений (максимальное значение не превышает допустимого)
     """
-    return result
+    if abs(min(samples)) > max(samples):
+        max_sample_value = abs(min(samples))
+    else:
+        max_sample_value = max(samples)
+    if max_sample_value == 0:  # Массив семплов состоит из одних нулей
+        return samples
+    normalize_multiplier = (2 ** (bits_per_sample - 1) - 1) / max_sample_value
+    normalized_samples = []
+    for sample in samples:
+        normalized_samples.append(int(sample * normalize_multiplier))
+    return normalized_samples
--- temp/example1_refactored.py
+++ temp/example1_refactored.py
@@ -2,7 +2,7 @@ import math
 import wave
 import struct
 
-SR = 44100
+SAMPLE_RATE = 44100
 
 
 def write_wave(filename, samples):
@@ -11,28 +11,48 @@ def write_wave(filename, samples):
     с частотой дискретизации SR в wav-файл
     """
     f = wave.open(filename, "w")
-    f.setparams((1, 2, SR, len(samples), "NONE", ""))
+    f.setparams((1, 2, SAMPLE_RATE, len(samples), "NONE", ""))
     f.writeframes(b"".join(
-        [struct.pack('<h', round(x * 32767)) for x in samples]))
+        [struct.pack('<h', x) for x in samples]))
     f.close()
 
 
-def num_samples(num_seconds: float):
+def num_samples(num_seconds: float) -> int:
     """
     Перевод секунд в количество сэмплов
     :param num_seconds: интервал времени, для которого считается количество семплов
-    :return: int количество семплов
+    :return: количество семплов
     """
-    return int(SR * num_seconds)
+    return int(SAMPLE_RATE * num_seconds)
 
 
 def generate_sine_wave(freq: int, duration: float):
     """ Сгенерировать семплы синусоиды с заданной частотой и длительностью звучания """
     samples = []
-    for sample_no in range(int(num_samples(duration))):
-        samples.append(math.sin(2 * math.pi * freq * sample_no / SR))
+    for sample_no in range(num_samples(duration)):
+        samples.append(math.sin(2 * math.pi * freq * sample_no / SAMPLE_RATE))
     return samples
 
 
+def normalize(samples: list, bits_per_sample=16) -> list:
+    """
+    Нормализует значения семплов в соответствии с разрядностью звукового устройства
+    :param samples: массив значений семплов
+    :param bits_per_sample: разрядность звукового устройства
+    :return: массив нормализованных значений (максимальное значение не превышает допустимого)
+    """
+    if abs(min(samples)) > max(samples):
+        max_sample_value = abs(min(samples))
+    else:
+        max_sample_value = max(samples)
+    if max_sample_value == 0:  # Массив семплов состоит из одних нулей
+        return samples
+    normalize_multiplier = (2 ** (bits_per_sample - 1) - 1) / max_sample_value
+    normalized_samples = []
+    for sample in samples:
+        normalized_samples.append(int(sample * normalize_multiplier))
+    return normalized_samples
+
+
 samples = generate_sine_wave(220, 1)
-write_wave("wave_test.wav", samples)
+write_wave("wave_test.wav", normalize(samples))
